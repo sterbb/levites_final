1245476ee2f9d163ee5e69407d3ec75d188f05f4
sterbb <webjrdivinagracia@gmail.com>
2023-05-15 15:03:46 +0800

    lyrics

--- views/modules/lyrics.php
+++ views/modules/lyrics.php
@@ -186,52 +186,42 @@
     </div>
 
 
-                <div class="col-12 col-xl-4 col-xxl-6 auth-cover-right  d-none d-xl-flex bg-light">
+                <div class="col-12 col-xl-6 col-xxl-6 auth-cover-right  d-none d-xl-flex bg-light">
 
                     <div class="card rounded-0 mb-0 border-0 bg-transparent">
                         <div class="card-body">
-                        <div class="video-container">
-                            <iframe width="600" height="300" class="rounded-3" src="https://www.youtube.com/embed/OjXkeHHPTzQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> </iframe>
-                        </div>
+                            <div class="video-container mb-5">
+                                <iframe width="600" height="300" class="rounded-3" src="https://www.youtube.com/embed/OjXkeHHPTzQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> </iframe>
+                            </div>
+                            
+                            <div class="row">
+                                <div class="d-flex align-items-center">
+                                    <h6>Playlists</h6>
+                                    <button type="button" data-bs-toggle="modal" data-bs-target="#AddPlaylist" class="btn btn-light border border-dark ms-3"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus text-success"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg></button>
 
-                        <h2 class="mt-3 p-4 " style="font-weight:bold">Playlist</h2>
-                            
-                            
+                                </div>
                                 
+                            </div>
+                       
+                            
+                            <hr>
+
+                            <div class="row">
+                                <div class="col-6 d-flex align-items-center">
+                                    <h2 class="mt-3 p-4 " style="font-weight:bold">Sunday Line Up</h2>
+                                    <button type="button" data-bs-toggle="modal" data-bs-target="#AddPlaylist" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus text-success"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg></button>
+                                </div>
+                            </div>
+                                    
+                         
+                        
                             <div class="bg-light " style="width:600; height:300;">
                                 <!-- Start DEMO HTML (Use the following code into your project)-->
-                            <div class="simple-audio-player " id="simp" data-config='{"shide_top":false,"shide_btm":false,"auto_load":false}'>
+                           
 
-                            <button type="button" data-bs-toggle="modal" data-bs-target="#AddPlaylist" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus text-success"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg></button>
-
-                            <button type="button" class="btn  btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-download-cloud text-warning"><polyline points="8 17 12 21 16 17"></polyline><line x1="12" y1="12" x2="12" y2="21"></line><path d="M20.88 18.09A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.29"></path></svg></button>
-                            <div class="simp-playlist">
-                                <ul>
-                                <li><span class="simp-source" data-src="http://www.archive.org/download/CanonInD_261/CanoninD.mp3">Living Hope</span><span class="simp-desc">Phil Wickham</span></li>
-                                <li><span class="simp-source" data-src="https://www.script-tutorials.com/demos/363/data/05.mp3">Artist 5</span></li>
-                                <li><span class="simp-source" data-src="https://www.dropbox.com/s/6pays95q2a9jrsf/2020.01.02.mp3?dl=1" data-cover="https://i.postimg.cc/sDBycWS8/NMS-2019.jpg">Nightmare Side</span><span class="simp-desc">Set Stori</span></li>
-                                <li class="simp-active"><span class="simp-source" data-src="https://www.dropbox.com/s/k011ag91uktonbv/Angklung%20Preman%20Pensiun.mp3?dl=1">Angklung Preman Pensiun</span></li>
-                                <li><span class="simp-source" data-src="https://www.dropbox.com/s/prgte33m86n9ce7/Canon%20Rock%20%28JerryC%29%20-%20funtwo.mp3?dl=1">Canon Rock (JerryC)</span><span class="simp-desc">by Funtwo</span></li>
-                                <li><span class="simp-source" data-src="http://listento.ardanradio.com:1059/;">105.9 FM Ardan Radio</span><span class="simp-desc">Streaming Radio</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/78_jailhouse-rock_elvis-presley-jerry-leiber-mike-stoller_gbia0080595b/Jailhouse%20Rock%20-%20Elvis%20Presley%20-%20Jerry%20Leiber-restored.mp3">Jailhouse Rock</span><span class="simp-desc">Elvis Presley</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/J._Period_and_Michael_Jackson_-_Man_Or_The_Music-2010/17%20Smooth%20Criminal.mp3">Smooth Criminal</span><span class="simp-desc">Michael Jackson</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/OldPop_256/VillagePeople-Y.m.c.a.mp3">Y.M.C.A</span><span class="simp-desc">Village People</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/chuckberryjohnnyb.goode/Chuck%20Berry%20-%20Johnny%20B.%20Goode.mp3">Johnny B. Goode</span><span class="simp-desc">Chuck Berry</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/AnimalsTheHouseOfTheRisingSun_201811/Animals%20-%20The%20House%20Of%20The%20Rising%20Sun.mp3">House of the Rising Sun</span><span class="simp-desc">The Animals</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/DontStopMeNow_255/Queen-DontStopMeNow.mp3">Don't Stop Me Now</span><span class="simp-desc">Queen</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/NeverGonnaGiveYouUpOriginal/Never%20Gonna%20Give%20You%20Up%20Original.mp3">Never Gonna Give You Up</span><span class="simp-desc">Rick Astley</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/gunsnrosessweetchildomine_202003/Guns%20N%27%20Roses%20-%20Sweet%20Child%20%27O%20Mine.mp3">Sweet Child O' Mine</span><span class="simp-desc">Guns N' Roses</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/01.TheFinalCountdown/01.%20The%20Final%20Countdown.mp3">The Final Countdown</span><span class="simp-desc">Europe</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/1MyHeartWillGoOnLoveThemeFromTitanic/1%20-%20My%20Heart%20Will%20Go%20On%20%28Love%20Theme%20from%20_Titanic_%29.mp3">My Heart Will Go On</span><span class="simp-desc">Céline Dion</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/tntvillage_323140/John%20Lennon%20-%20Imagine/01%20Imagine.mp3">Imagine</span><span class="simp-desc">John Lennon</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/TheBeatles1970HeyJudeAppleRecords6e062.0434807HeyJude/The%20Beatles%20-%201970%20-%20Hey%20Jude%20%28Apple%20Records%3B%20%236e%20062.04348%29_07%20-%20Hey%20Jude.mp3">Hey Jude</span><span class="simp-desc">The Beatles</span></li>
-                                <li><span class="simp-source" data-src="https://archive.org/download/TakeMeHomeCountryRoad/JohnDenver-TakeMeHomeCountryRoad.mp3">Take Me Home, Country Roads</span><span class="simp-desc">John Denver</span></li>
-                                </ul>
-                            </div>
-                            <div class="simp-footer">Made with 💖 &amp; 🙌 by <a href="https://bit.ly/sekedus" target="_blank" title="Sekedus">Sekedus</a></div>
-                            </div>    
-                                <!-- END EDMO HTML (Happy Coding!)-->
-                            </div>
+                           
+                            
+                           
 
                                              
                                         
@@ -353,52 +343,33 @@
                     <div class="modal-dialog modal-dialog-centered">
                       <div class="modal-content">
                         <div class="modal-header ">
-                          <h5 class="modal-title ">Favorite Songs</h5>
+                          <h5 class="modal-title ">Sunday Worship</h5>
                           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                             <div class="form-body g-5 ">
                                 <div class="row g-3  "> 
-                                    <div class="row mt-3 col-12">
+                                    <div class="row mt-3 col-12 text-center">
                                         <ul>
-                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="http://www.archive.org/download/CanonInD_261/CanoninD.mp3">Living Hope</span><span class="simp-desc">Phil Wickham</span>
+                                            <p class=" " type="text" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="http://www.archive.org/download/CanonInD_261/CanoninD.mp3">Living Hope</span><span class="simp-desc">Phil Wickham</span>
                                         </ul>
                                         <ul>
-                                             <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="https://www.script-tutorials.com/demos/363/data/05.mp3">Artist 5</span>
+                                             <p class=" " type="text" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="https://www.script-tutorials.com/demos/363/data/05.mp3">Artist 5</span>
                                         </ul>
                                         <ul>   
-                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="https://www.dropbox.com/s/6pays95q2a9jrsf/2020.01.02.mp3?dl=1" data-cover="https://i.postimg.cc/sDBycWS8/NMS-2019.jpg">Nightmare Side</span><span class="simp-desc">Set Stori</span>
+                                            <p class=" " type="text" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="https://www.dropbox.com/s/6pays95q2a9jrsf/2020.01.02.mp3?dl=1" data-cover="https://i.postimg.cc/sDBycWS8/NMS-2019.jpg">Nightmare Side</span><span class="simp-desc">Set Stori</span>
                                        </ul>
                                        <ul>
-                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="https://www.dropbox.com/s/k011ag91uktonbv/Angklung%20Preman%20Pensiun.mp3?dl=1">Angklung Preman Pensiun</span>
+                                            <p class=" " type="text" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="https://www.dropbox.com/s/k011ag91uktonbv/Angklung%20Preman%20Pensiun.mp3?dl=1">Angklung Preman Pensiun</span>
                                         </ul> 
                                         <ul>
-                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="https://www.dropbox.com/s/prgte33m86n9ce7/Canon%20Rock%20%28JerryC%29%20-%20funtwo.mp3?dl=1">Canon Rock (JerryC)</span><span class="simp-desc">by Funtwo</span>
+                                            <p class=" " type="text" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="https://www.dropbox.com/s/prgte33m86n9ce7/Canon%20Rock%20%28JerryC%29%20-%20funtwo.mp3?dl=1">Canon Rock (JerryC)</span><span class="simp-desc">by Funtwo</span>
                                        </ul>    
                                        <ul>   
-                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="http://listento.ardanradio.com:1059/;">105.9 FM Ardan Radio</span><span class="simp-desc">Streaming Radio</span>
+                                            <p class=" " type="text" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="http://listento.ardanradio.com:1059/;">105.9 FM Ardan Radio</span><span class="simp-desc">Streaming Radio</span>
                                          </ul>
                                     </div>  
-                                    <div class="row col-12">
-                                        <ul>
-                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="http://www.archive.org/download/CanonInD_261/CanoninD.mp3">Living Hope</span><span class="simp-desc">Phil Wickham</span>
-                                        </ul>
-                                        <ul>
-                                             <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="https://www.script-tutorials.com/demos/363/data/05.mp3">Artist 5</span>
-                                        </ul>
-                                        <ul>   
-                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="https://www.dropbox.com/s/6pays95q2a9jrsf/2020.01.02.mp3?dl=1" data-cover="https://i.postimg.cc/sDBycWS8/NMS-2019.jpg">Nightmare Side</span><span class="simp-desc">Set Stori</span>
-                                       </ul>
-                                       <ul>
-                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="https://www.dropbox.com/s/k011ag91uktonbv/Angklung%20Preman%20Pensiun.mp3?dl=1">Angklung Preman Pensiun</span>
-                                        </ul> 
-                                        <ul>
-                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="https://www.dropbox.com/s/prgte33m86n9ce7/Canon%20Rock%20%28JerryC%29%20-%20funtwo.mp3?dl=1">Canon Rock (JerryC)</span><span class="simp-desc">by Funtwo</span>
-                                       </ul>    
-                                       <ul>   
-                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><span class="simp-source p-3" data-src="http://listento.ardanradio.com:1059/;">105.9 FM Ardan Radio</span><span class="simp-desc">Streaming Radio</span>
-                                         </ul>
-                                    </div> 
+    
                                    
                                 </div>
                             </div>
