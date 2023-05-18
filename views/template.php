d65b782d2ea637fd8adfcb32aca759909bf593ee
JC-MOYA <123946987+JC-MOYA@users.noreply.github.com>
2023-05-18 15:48:27 +0800

    Update template.php

--- views/template.php
+++ views/template.php
@@ -52,13 +52,13 @@
 			$_GET["route"] == 'forgotpassword' ||
 			$_GET["route"] == 'resetpassword' ||	
 			$_GET["route"] == 'accounts' ||
-            $_GET["route"] == 'publichomepage' ||
-            $_GET["route"] == 'churchpage'||
-            $_GET["route"] == 'catdetails'||
-            $_GET["route"] == 'adminhomepage'||
-            $_GET["route"] == 'reportgen'||
-            $_GET["route"] == 'churchcalendar' ||
-            $_GET["route"] == 'songlist' || 
+			$_GET["route"] == 'publichomepage' ||
+			$_GET["route"] == 'churchpage'||
+			$_GET["route"] == 'catdetails'||
+			$_GET["route"] == 'adminhomepage'||
+			$_GET["route"] == 'reportgen'||
+			$_GET["route"] == 'churchcalendar' ||
+			$_GET["route"] == 'songlist' || 
 			$_GET["route"] == 'filestorage' || 
 			$_GET["route"] == 'lyrics'|| 
 			$_GET["route"] == 'requests'|| 
@@ -66,12 +66,12 @@
 			$_GET["route"] == 'playlist'||
 			$_GET["route"] == 'profile'||
 			$_GET["route"] == 'superuser'||
-			$_GET["route"] == 'publicregistration'||
-			$_GET["route"] == 'landingpage'){
+			$_GET["route"] == 'publicregistration' ||
+			$_GET["route"] == 'requestPassword'){
 
             include "modules/".$_GET["route"].".php";
 
-			if($_GET["route"] == 'login' || $_GET["route"] == 'churchregistration'  ||$_GET["route"] == 'publicregistration'  ||$_GET["route"] == 'forgotpassword' ||$_GET["route"] == 'resetpassword'){
+			if($_GET["route"] == 'login' || $_GET["route"] == 'churchregistration' || $_GET["route"] == 'requestPassword'  || $_GET["route"] == 'publicregistration'  ||$_GET["route"] == 'forgotpassword' ||$_GET["route"] == 'resetpassword'){
 			
 			}else{
 				include "modules/header.php";
@@ -100,7 +100,7 @@
 
     
     ?>
-
+	
 
 
 
