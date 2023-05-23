76981c8e1005c897c3a361bfe175da7bb3bbd690
JC-MOYA <123946987+JC-MOYA@users.noreply.github.com>
2023-05-24 00:14:48 +0800

    Update publicregistration.php

--- views/modules/publicregistration.php
+++ views/modules/publicregistration.php
@@ -1,110 +1,107 @@
 
-    <div class="bg-light">
-      <div class="col d-flex justify-content-center ">
-
-    
-
-        <div class="col-12 col-xl-5 col-xxl-6   ">
+<div class="row p-5 d-flex justify-content-center align-items-center">
+        <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 col-xxl-6">
           <div class="card rounded-0 m-3 border-0 rounded-3 ">
-            <div class="card-body p-sm-10">
+            <div class="card-body ">
                 <div class="text-center">
               <img src="views/img/LEVITES.png" class="mb-4" width="100" alt="">
+              
               <h4 class="fw-bold">Registration</h4>
 
                 <div>
-                    <button type="button" class="btn btn-dark"><a  href="churchregistration"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-repeat text-warning"><polyline points="17 1 21 5 17 9"></polyline><path d="M3 11V9a4 4 0 0 1 4-4h14"></path><polyline points="7 23 3 19 7 15"></polyline><path d="M21 13v2a4 4 0 0 1-4 4H3"></path></svg></button>
+                    <button type="button" class="btn "><a  href="churchregistration"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-repeat text-warning"><polyline points="17 1 21 5 17 9"></polyline><path d="M3 11V9a4 4 0 0 1 4-4h14"></path><polyline points="7 23 3 19 7 15"></polyline><path d="M21 13v2a4 4 0 0 1-4 4H3"></path></svg></button>
                             <label class="form-check-label mt-1 text-warning" for="flexSwitchCheckChecked" style="display: inline-block">Switch Church Registration to Public Registration</label></a>
                     </div>
              
             </div>
 
-                <div class="form-body g-3">
-                    <form role="form" id="church-form" method="POST" autocomplete="nope" class="row g-3">
-                
-                            <div class="row g-3">
+              <div class="form-body g-3">
+                <form role="form" id="church-form" method="POST" autocomplete="nope" class="row g-3">
+            
+                        <div class="row g-3">
+                        <div class="col-6">
+                            <label for="inputUsername" class="form-label">Username *</label>
+                            <input type="email" class="form-control border-3" id="inputUsername" placeholder="Jhon">
+                        </div>
+
                             <div class="col-6">
-                                <label for="inputUsername" class="form-label">Username</label>
-                                <input type="email" class="form-control border-3" id="inputUsername" placeholder="Jhon">
-                            </div>
-
-                                <div class="col-6">
-                                <label for="inputChoosePassword" class="form-label">Password</label>
-                                <div class="input-group" id="show_hide_password">
-                                    <input type="password" class="form-control border-end-0 border-3" id="inputChoosePassword" value="12345678" placeholder="Enter Password">
-                                    <a href="javascript:;" class="input-group-text bg-transparent border-3"><i class="bi bi-eye-slash-fill"></i></a>
-                                    </div>
-                                </div>     
-                                
-                                <div class="col-12">
-                                <label for="inputEmailAddress" class="form-label">Email Address</label>
-                                <input type="email" class="form-control border-3" id="inputEmailAddress" placeholder="example@user.com">
-                                </div> 
-
-                            </div>
-
-                            <div class="row g-3">
-
-                                <div class="col-6">
-                                    <label for="inputName" class="form-label">Name</label>
-                                    <input type="text" class="form-control border-3" id="inputName" placeholder="JAY COBB">
+                            <label for="inputChoosePassword" class="form-label">Password *</label>
+                            <div class="input-group" id="show_hide_password">
+                                <input type="password" class="form-control border-end-0 border-3" id="inputChoosePassword" value="12345678" placeholder="Enter Password">
+                                <a href="javascript:;" class="input-group-text  border-3"><i class="bi bi-eye-slash-fill"></i></a>
                                 </div>
-
-                                <div class="col-6">
-                                    <label for="inputLastName" class="form-label">Last Name</label>
-                                    <input type="text" class="form-control border-3" id="inputLastName" placeholder="MOYA">
-                                </div>
-
-
-                                <div class="col-6">
-                                    <label for="inputReligion" class="form-label">Religion</label>
-                                    <select class="form-select border-3" id="inputSelectCountry" aria-label="Default select example">
-                                    <option selected="" value="Catholic">Catholic</option>
-                                    <option value="Baptist">Baptist</option>
-                                    <option value="Christian ">Christian</option>
-                                    </select>
-                                </div>
-
-                                <div class="col-6">
-                                    <label for="inputNum" class="form-label">Telephone Number</label>
-                                    <input type="text" class="form-control border-3" id="inputNum" placeholder="432-0048">
-                                </div>
-
+                            </div>     
                             
+                            <div class="col-12">
+                            <label for="inputEmailAddress" class="form-label">Email Address *</label>
+                            <input type="email" class="form-control border-3" id="inputEmailAddress" placeholder="example@user.com">
+                            </div> 
+
+                        </div>
+
+                        <div class="row g-3">
+
+                            <div class="col-6">
+                                <label for="inputName" class="form-label">Name *</label>
+                                <input type="text" class="form-control border-3" id="inputName" placeholder="JAY COBB">
                             </div>
 
-                            <div class="row g-3">
+                            <div class="col-6">
+                                <label for="inputLastName" class="form-label">Last Name *</label>
+                                <input type="text" class="form-control border-3" id="inputLastName" placeholder="MOYA">
+                            </div>
 
-                                <div class="col-12 ">
-                                    <div class="form-check form-switch border-3">
-                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked">
-                                    <label class="form-check-label" for="flexSwitchCheckChecked">I read and agree to Terms &amp; Conditions</label>
-                                    <a type="button" data-bs-toggle="modal" data-bs-target="#LevitesAgreement"><i class="lni lni-question-circle text-primary"></i></a>
-                                    </div>
-                                </div>
-                                
-                                
 
-                                <div class="col-12">
-                                    <div class="d-grid">
-                                        <a href="login" class="btn btn-primary border-3">Register</a>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="text-start">
-                                    <p class="mb-0">Already have an account? <a href="login">Sign in here</a></p>
-                                    </div>
+                            <div class="col-6">
+                                <label for="inputReligion" class="form-label">Religion *</label>
+                                <select class="form-select border-3" id="inputSelectCountry" aria-label="Default select example">
+                                <option selected="" value="Catholic">Catholic</option>
+                                <option value="Baptist">Baptist</option>
+                                <option value="Christian ">Christian</option>
+                                </select>
+                            </div>
+
+                            <div class="col-6">
+                                <label for="inputNum" class="form-label">Telephone Number *</label>
+                                <input type="text" class="form-control border-3" id="inputNum" placeholder="432-0048">
+                            </div>
+
+                        
+                        </div>
+
+                        <div class="row g-3">
+
+                            <div class="col-12 ">
+                                <div class="form-check form-switch border-3">
+                                <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked">
+                                <label class="form-check-label" for="flexSwitchCheckChecked">I read and agree to Terms &amp; Conditions</label>
+                                 <a type="button" data-bs-toggle="modal" data-bs-target="#LevitesAgreement"><i class="lni lni-question-circle text-primary"></i></a>
                                 </div>
                             </div>
-    
-                    </form>
-                </div>
+                            
+                            
+
+                            <div class="col-12">
+                                <div class="d-grid">
+                                    <a href="login" class="btn btn-primary border-3">Register</a>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="text-start">
+                                <p class="mb-0">Already have an account? <a href="login">Sign in here</a></p>
+                                </div>
+                            </div>
+                        </div>
+  
+                </form>
+              </div>
 
           </div>
           </div>
         </div>
      </div>
       <!--end row-->
-</div>
+
 
 <!--Modal-->
 <div class="col">
