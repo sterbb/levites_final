5480650946d87da6bae0e6a61c3f006444347fc7
JC-MOYA <123946987+JC-MOYA@users.noreply.github.com>
2023-05-23 23:40:00 +0800

    Update resetpassword.php
    Moya reset update

--- views/modules/resetpassword.php
+++ views/modules/resetpassword.php
@@ -1,32 +1,35 @@
 <!--authentication-->
+<div class="container-fluid" style="margin-top:130px;">
+        <div class="row">
+            <div class="d-flex justify-content-center align-items-center">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-4 col-sm-10 ">
+                    <div class="card border-3 rounded-5">
+                        <div class="card-body p-5">
+                            <div class=" text-center">
+                                <img src="views/images/logo.png" class="align-center mb-4" width="100" alt="">
+                                <h4 class="fw-bold">Genrate New Password</h4>
+                                <p class="mb-0">We received your reset password request. Please enter your new password!</p>
+                            </div>
 
-<div class="container my-5">
-    <div class="row">
-        <div class="col-12 col-md-8 col-lg-6 col-xl-5 col-xxl-4 mx-auto">
-            <div class="card border-3">
-                <div class="card-body p-5">
-                    <img src="assets/images/logo-icon.png" class="mb-4" width="45" alt="">
-                    <h4 class="fw-bold">Genrate New Password</h4>
-                    <p class="mb-0">We received your reset password request. Please enter your new password!</p>
-                    <div class="form-body mt-4">
-                        <form class="row g-3">
-                            <div class="col-12">
-                                <label class="form-label" for="NewPassword">New Password</label>
-                                <input type="text" class="form-control" id="NewPassword" placeholder="Enter new password">
-                            </div>
-                            <div class="col-12">
-                                <label class="form-label" for="ConfirmPassword">Confirm Password</label>
-                                <input type="text" class="form-control" id="ConfirmPassword" placeholder="Confirm password">
-                            </div>
-                            <div class="col-12">
-                                <div class="d-grid gap-2">
-                                <a href="login" class="btn btn-primary">Change Password</a>
-                                <a href="login" class="btn btn-light">Back to Login</a>
-                            </div>
-                            </div>
-                        </form>
+                        <div class="form-body mt-4">
+                            <form class="row g-3">
+                                <div class="col-12">
+                                    <label class="form-label" for="NewPassword">New Password</label>
+                                    <input type="text" class="form-control" id="NewPassword" placeholder="Enter new password">
+                                </div>
+                                <div class="col-12">
+                                    <label class="form-label" for="ConfirmPassword">Confirm Password</label>
+                                    <input type="text" class="form-control" id="ConfirmPassword" placeholder="Confirm password">
+                                </div>
+                                <div class="col-12">
+                                    <div class="d-grid gap-2">
+                                        <a href="login" type="button"  class="btn text-white" style="background:linear-gradient(150deg, #AAC4F2, #C9B4ED 100%); font-weight:bold;">Change Password</a>
+                                        <a href="login" type="button" class="btn font-bold" style="font-weight:bold;">Back to Login</a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-
                 </div>
             </div>
         </div>
